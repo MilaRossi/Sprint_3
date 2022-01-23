86c6b6ac86db758b4e00eb391c27e6590c76ef8a
MilaRossi <liudmila.rossi@gmail.com>
2022-01-23 12:27:26 +0100

    added tests courier login 200, 400, 404 moved methods create, login, delete to class GenerateLoginPasswordForCourier

--- src/test/java/ru/yandex/praktikum/CourierLoginAndCheckResponseTest.java
+++ src/test/java/ru/yandex/praktikum/CourierLoginAndCheckResponseTest.java
@@ -1,4 +1,63 @@
 package ru.yandex.praktikum;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CourierLoginAndCheckResponseTest {
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+    }
+
+    @Test
+    public void loginCourierAndCheckResponse200() {
+        // создание пары логин-пароль
+        GenerateLoginPasswordForCourier courier = new GenerateLoginPasswordForCourier();
+        JSONObject credentials = courier.generateLoginPassword();
+        // создание курьера
+        courier.createCourier(credentials)
+                .then().assertThat().statusCode(201)
+                .and()
+                .assertThat().body("ok", equalTo(true));
+        // логин курьера
+        Response response = courier.loginCourier(credentials);
+        response.then().assertThat().statusCode(200);
+        // извлекаем id курьера
+        Integer id = response.then().extract().path("id");
+        assertThat("id",notNullValue());
+        // удаляем курьера
+        courier.deleteCourier(id).then().assertThat().statusCode(200)
+                .and()
+                .assertThat().body("ok", equalTo(true));
+
+    }
+
+    @Test
+    public void loginCourierAndCheckResponse400() {
+        // создание пары логин - пустой пароль
+        GenerateLoginPasswordForCourier courier = new GenerateLoginPasswordForCourier();
+        JSONObject json = courier.generateEmptyPassword();
+        // запрос без пароля
+        Response response = courier.loginCourier(json);
+        response.then().assertThat().statusCode(400).and().assertThat().body("message",equalTo("Недостаточно данных для входа"));
+    }
+
+    @Test
+    public void loginCourierAndCheckResponse404() {
+        // создание пары логин - пароль
+        GenerateLoginPasswordForCourier courier = new GenerateLoginPasswordForCourier();
+        JSONObject json = courier.generateLoginPassword();
+        // запрос с несуществующей парой логин - пароль
+        Response response = courier.loginCourier(json);
+        response.then().assertThat().statusCode(404).and().assertThat().body("message",equalTo("Учетная запись не найдена"));
+    }
+
+
+
 }
--- src/test/java/ru/yandex/praktikum/GenerateLoginPasswordForCourier.java
+++ src/test/java/ru/yandex/praktikum/GenerateLoginPasswordForCourier.java
@@ -1,10 +1,15 @@
 package ru.yandex.praktikum;
 
+import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONObject;
 
+import static io.restassured.RestAssured.given;
+
 
 public class GenerateLoginPasswordForCourier {
+
+
     public JSONObject generateLoginPassword() {
         String courierLogin = RandomStringUtils.randomAlphabetic(10);
         String courierPassword = RandomStringUtils.randomAlphabetic(10);
@@ -21,6 +26,22 @@ public class GenerateLoginPasswordForCourier {
         json.put("login", courierLogin);
         json.put("password","" );
         return json;
+    }
+
+    public Response createCourier(JSONObject creds ) {
+        return given().header("Content-type", "application/json")
+                .body(creds.toString()).when().post("/api/v1/courier");
+    }
+
+
+    public Response loginCourier(JSONObject creds ) {
+        return given().header("Content-type", "application/json")
+                .body(creds.toString()).when().post("/api/v1/courier/login");
+    }
+
+    public Response deleteCourier(Integer id) {
+        return given().header("Content-type", "application/json")
+                .delete("/api/v1/courier/" + id.toString());
 
     }
 
