7bcbc8c0db9b512dafb54514bc75bc759b251e33
MilaRossi <liudmila.rossi@gmail.com>
2022-01-23 12:43:34 +0100

    added delete courier

--- src/test/java/ru/yandex/praktikum/CreateCourierAndCheckResponseTest.java
+++ src/test/java/ru/yandex/praktikum/CreateCourierAndCheckResponseTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CreateCourierAndCheckResponseTest {
 
@@ -17,33 +19,65 @@ public class CreateCourierAndCheckResponseTest {
 
     @Test
     public void createCourierAndCheckResponse201() {
+        // создание пары логин-пароль
         GenerateLoginPasswordForCourier courier = new GenerateLoginPasswordForCourier();
-        JSONObject json = courier.generateLoginPassword();
-        Response response = given().header("Content-type", "application/json").and().body(json.toString()).when().post("/api/v1/courier");
-        response.then().assertThat().statusCode(201)
+        JSONObject credentials = courier.generateLoginPassword();
+        // создание курьера
+        Response response1 = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(credentials.toString())
+                .when()
+                .post("/api/v1/courier");
+        response1.then().assertThat().statusCode(201)
             .and().assertThat().body("ok", equalTo(true));
+        // логин курьера
+        Response response2 = courier.loginCourier(credentials);
+        response2.then().assertThat().statusCode(200);
+        // извлекаем id курьера
+        Integer id = response2.then().extract().path("id");
+        assertThat("id",notNullValue());
+        // удаляем курьера
+        courier.deleteCourier(id).then().assertThat().statusCode(200)
+                .and()
+                .assertThat().body("ok", equalTo(true));
+
 
     }
 
     @Test
     public void createCourierAndCheckResponse400() {
+        // создание пары логин - пустой пароль
         GenerateLoginPasswordForCourier courier = new GenerateLoginPasswordForCourier();
-        JSONObject json = courier.generateEmptyPassword();
-        Response response = given().header("Content-type", "application/json").and().body(json.toString()).when().post("/api/v1/courier");
-        response.then().assertThat().statusCode(400).
-            and().assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"));
+        JSONObject credentials = courier.generateEmptyPassword();
+        // создание курьера
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(credentials.toString())
+                .when()
+                .post("/api/v1/courier");
+        response.then().assertThat().statusCode(400)
+                        .and().assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"));
 
     }
 
 
     @Test
     public void createCourierAndCheckResponse409() {
+        // создание пары логин-пароль
         GenerateLoginPasswordForCourier courier = new GenerateLoginPasswordForCourier();
-        JSONObject json = courier.generateLoginPassword();
-        Response responseFirst = given().header("Content-type", "application/json").and().body(json.toString()).when().post("/api/v1/courier");
+        JSONObject credentials = courier.generateLoginPassword();
+        // создание курьера
+        Response responseFirst = given().header("Content-type", "application/json")
+                .and()
+                .body(credentials.toString())
+                .when()
+                .post("/api/v1/courier");
         responseFirst.then().assertThat().statusCode(201)
                 .and().assertThat().body("ok", equalTo(true));
-        Response responseSecond = given().header("Content-type", "application/json").and().body(json.toString()).when().post("/api/v1/courier");
+        // повторное создание курьера
+        Response responseSecond = given().header("Content-type", "application/json").and().body(credentials.toString()).when().post("/api/v1/courier");
         responseSecond.then().assertThat().statusCode(409)
             .and().assertThat().body("message", equalTo("Этот логин уже используется. Попробуйте другой."));
 
